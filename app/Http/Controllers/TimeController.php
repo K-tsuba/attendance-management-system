338925eba7f36be11f7cd96819062f5c611bd298
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2022-04-08 17:11:26 +0900

    勤怠時間の表示完了

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -55,4 +55,26 @@ class TimeController extends Controller
             return redirect('/');
         }
     }
+    
+    public function show(Time $time)
+    {
+        // //先週のランキング
+        // $Users = User::get();
+        // $last_monday = Carbon::today()->startOfWeek()->subDay(7)->toDateString();
+        // $this_monday = Carbon::today()->startOfWeek()->toDateString();
+        // $week_ranking = $time->ranking($Users, $last_monday, $this_monday);
+        // //先月のランキング
+        // $last_month = Carbon::today()->startOfMonth()->subMonth()->toDateString();
+        // $this_month = Carbon::today()->startOfMonth()->toDateString();
+        // $month_ranking = $time->ranking($Users, $last_month, $this_month);
+        
+        // $rest_sum = strtotime($time->rest_stop) - strtotime($time->rest_start);
+        // $rest_time = gmdate('H:i:s', $rest_sum);
+        
+        return view('times/show')->with([
+            'times' => $time->get()
+            // 'week_ranking' => $week_ranking,
+            // 'month_ranking' => $month_ranking
+        ]);
+    }
 }
---
+++ resources/views/times/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content')
+<body>
+    <h1>勤務時間管理画面</h1>
+    @foreach ($times as $time)
+        <div>
+            @if(isset( $time ))
+            <p>User Name : {{ $time->user->name }}</p>
+            @else
+            <p>User Name : name</p>
+            @endif
+        </div>
+        <div>
+            <p>DATE : {{ $time->updated_at }}</p>
+        </div>
+        <div>
+            <p>Work Time : {{ $time->time }}</p>
+        </div>
+        <div>
+            <p>Rest Time : {{ $time->rest_time($time) }}</p>
+        </div>
+    @endforeach
+</body>
+@endsection
