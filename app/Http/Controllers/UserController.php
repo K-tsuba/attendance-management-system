107f0c8ceabb35f643261df5217bab85f0323ee8
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2022-04-09 00:45:14 +0900

    出勤・退勤状況画面の作成

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Time;
+
+class UserController extends Controller
+{
+    public function index(Time $time)
+    {
+        $today = Carbon::today();
+        // $time = $time->whereDate('updated_at', $today)->get();
+        $time = $time->get();
+        return view('/users/index')->with([
+            'times' => $time
+        ]);
+    }
+}
