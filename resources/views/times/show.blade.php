de9102fd7bac435b00c09255277c866e5d5a4831
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2022-05-04 00:59:17 +0900

    勤務時間確認画面レイアウト編集

--- resources/views/times/show.blade.php
+++ resources/views/times/show.blade.php
@@ -1,25 +1,32 @@
 @extends('layouts.app')
 @section('content')
 <body>
-    <h1>勤務時間管理画面</h1>
-    @foreach ($times as $time)
-        <div>
-            @if(isset( $time ))
-            
-            <p>User Name : {{ $time->user->name }}</p>
-            @else
-            <p>User Name : name</p>
-            @endif
-        </div>
-        <div>
-            <p>DATE : {{ $time->updated_at }}</p>
-        </div>
-        <div>
-            <p>Work Time : {{ $time->time }}</p>
-        </div>
-        <div>
-            <p>Rest Time : {{ $time->rest_time($time) }}</p>
-        </div>
-    @endforeach
+    <div class="container">
+        <h1>勤務時間管理</h1>
+        <table class="table">
+            <thead class="thead-light">
+                <tr>
+                    <th>User Name</th>
+                    <th>Date</th>
+                    <th>Work Time</th>
+                    <th>Rest Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($times as $time)
+                    <tr>
+                        @if(isset( $time ))
+                        <td>{{ $time->user->name }}</td>
+                        @else
+                        <td>Name</td>
+                        @endif
+                        <td>{{ $time->updated_at }}</td>
+                        <td>{{ $time->time }}</td>
+                        <td>{{ $time->rest_time($time) }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 @endsection
